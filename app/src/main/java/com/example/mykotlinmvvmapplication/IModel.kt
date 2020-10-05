1d40f4888532f0e3363e37e8e0eb19aa65f7012e
geraa1985 <geraa1985@gmail.com>
2020-10-05 12:00:31 +0300

    Add model with interface

---
+++ app/src/main/java/com/example/mykotlinmvvmapplication/IModel.kt
@@ -0,0 +1,5 @@
+package com.example.mykotlinmvvmapplication
+
+interface IModel {
+    fun getdata(): String
+}
---
+++ app/src/main/java/com/example/mykotlinmvvmapplication/Model.kt
@@ -0,0 +1,9 @@
+package com.example.mykotlinmvvmapplication
+
+class Model : IModel {
+
+    override fun getdata(): String {
+        return "Hello!"
+    }
+
+}
