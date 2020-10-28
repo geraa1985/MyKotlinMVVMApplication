ebb33c224086b65080cc2740cf7a668024b321b0
geraa1985 <geraa1985@gmail.com>
2020-10-28 12:39:01 +0300

    Refactor ViewHolder

--- app/src/main/java/com/example/mykotlinmvvmapplication/presentation/adapters/NotesRVAdapter.kt
+++ app/src/main/java/com/example/mykotlinmvvmapplication/presentation/adapters/NotesRVAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.mykotlinmvvmapplication.presentation.adapters
 
+import android.content.res.Resources
 import android.graphics.Paint
 import android.view.LayoutInflater
 import android.view.View
@@ -11,7 +12,8 @@ import com.example.mykotlinmvvmapplication.R
 import com.example.mykotlinmvvmapplication.domain.entities.Note
 import com.example.mykotlinmvvmapplication.presentation.extentions.getColor
 import com.example.mykotlinmvvmapplication.presentation.viewmodels.MainViewModel
-import kotlinx.android.synthetic.main.item_note.view.*
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_note.*
 
 class NotesRVAdapter(val viewModel: MainViewModel) : RecyclerView.Adapter<NotesRVAdapter.ViewHolder>() {
 
@@ -34,20 +36,21 @@ class NotesRVAdapter(val viewModel: MainViewModel) : RecyclerView.Adapter<NotesR
 
     override fun getItemCount() = notes.size
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind(note: Note) = itemView.run {
+    inner class ViewHolder(override val containerView: View) :
+            RecyclerView.ViewHolder(containerView), LayoutContainer {
+
+        fun bind(note: Note) {
             note.run {
                 tv_title.text = title
                 tv_title.paintFlags = Paint.UNDERLINE_TEXT_FLAG
                 tv_text.text = text
-                (itemView as CardView).setCardBackgroundColor(ResourcesCompat.getColor(resources, getColor(), null))
+                (itemView as CardView).setCardBackgroundColor(ResourcesCompat.getColor(Resources.getSystem(), getColor(), null))
 
                 itemView.setOnClickListener {
                     viewModel.clickOnNote(id)
                 }
             }
-
         }
-    }
 
+    }
 }
